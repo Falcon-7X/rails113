4fdea0b2f15722d013af45f15e8cf318a73db59c
kai <328458211@qq.com>
2017-03-20 14:30:11 +0800

    add find_params_and_check_permission in postscontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :find_params_and_check_permission, :only => [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,23 +21,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-    @post.group = @group
 
-    if current_user != @post.user
-      redirect_to root_path, alert: "You have no permission!"
-    end
   end
 
   def update
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-    @post.group = @group
-
-    if current_user != @post.user
-      redirect_to root_path, alert: "You have no permission!"
-    end
 
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "update success!"
@@ -46,6 +34,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
+
+    @post.destroy
+    redirect_to account_posts_path(@post), alert: "成功删除文章！"
+  end
+
+  def find_params_and_check_permission
     @group = Group.find(params[:group_id])
     @post = Post.find(params[:id])
     @post.group = @group
@@ -53,9 +47,6 @@ class PostsController < ApplicationController
     if current_user != @post.user
       redirect_to root_path, alert: "You have no permission!"
     end
-
-    @post.destroy
-    redirect_to account_posts_path(@post), alert: "成功删除文章！"
   end
 
   private
