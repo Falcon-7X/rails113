b493404a331746f2de3217f9278806f261ece7a8
kai <328458211@qq.com>
2017-03-20 14:25:04 +0800

    add post#edit/update/destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -17,6 +17,45 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
+
+    @post.destroy
+    redirect_to account_posts_path(@post), alert: "成功删除文章！"
   end
 
   private
