bdd6a108e574bd509fd113ebbdd2d0f4a75e6d95
kai <328458211@qq.com>
2017-03-30 15:10:16 +0800

    rename group to movie and rename post to prview

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -60,7 +60,7 @@ class GroupsController < ApplicationController
 
     if !current_user.is_member_of?(@group)
       current_user.join!(@group)
-      flash[:notice] = "加入本讨论版成功！"
+      flash[:notice] = "收藏成功！"
     else
       flash[:warning] = "你已经是本讨论版成员了！"
     end
@@ -73,7 +73,7 @@ class GroupsController < ApplicationController
 
     if current_user.is_member_of?(@group)
       current_user.quit!(@group)
-      flash[:alert] = "已退出本讨论版！"
+      flash[:alert] = "取消收藏！"
     else
       flash[:warning] = "你不是本讨论版成员！"
 
